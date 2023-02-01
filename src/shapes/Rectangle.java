e47de365d23061404130a73ad2a932cb2acbeb1e
Javier Razo <javier.razo1221@gmail.com>
2023-02-01 14:59:23 -0700

    added square

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,14 @@
+public class Rectangle {
+    protected int length;
+    protected int width;
+    protected int side;
+//        Rectangle should define a constructor that accepts two numbers for length and width, and sets those properties.
+public Rectangle(int widthNum, int lengthNum) {
+    this.width = widthNum;
+    this.length = lengthNum;
+}
+public int getArea() {
+    return width * length;
+}
+public int getPerimeter() {
+    return (2 * length) + (2 * width);
